b09ebeb87082ffea5a3a737b1c2816561ebb40c9
holaholu <olaoluhimself@yahoo.com>
2025-11-26 11:06:50 -0600

    Practice edit

---
+++ quick.py
@@ -0,0 +1,9 @@
+def firstUniqChar(s: str) -> int:
+        for val in s:
+            if s.count(val) == 1:
+                print(f"Found unique character: {val}")
+                return s.index(val)
+        return -1
+
+
+print(firstUniqChar("leetcode"))
